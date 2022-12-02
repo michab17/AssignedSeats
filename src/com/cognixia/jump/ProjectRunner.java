3b823805e544e629e0bbafe7499142e0948fbb6b
Micha Bernhard <mib70@pitt.edu>
2022-12-02 12:44:42 -0700

    Move Scanner outside of main function

--- src/com/cognixia/jump/ProjectRunner.java
+++ src/com/cognixia/jump/ProjectRunner.java
@@ -4,86 +4,119 @@ import java.util.Scanner;
 
 public class ProjectRunner {
 
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		char theater[][] = {
-				{'O', 'O', 'O', 'O', 'O'},
-				{'O', 'O', 'O', 'O', 'O'},
-				{'O', 'O', 'O', 'O', 'O'},
-				{'O', 'O', 'O', 'O', 'O'},
-				{'O', 'O', 'O', 'O', 'O'}
-		};
-		
+
+		char theater[][] = { { 'O', 'O', 'O', 'O', 'O' }, { 'O', 'O', 'O', 'O', 'O' }, { 'O', 'O', 'O', 'O', 'O' },
+				{ 'O', 'O', 'O', 'O', 'O' }, { 'O', 'O', 'O', 'O', 'O' } };
+
 		printTheater(theater);
-		
+
 		int[] seat = userInputForSeat();
 		String name = userInputForName();
-		
-		System.out.println("The seat you chose was:\n" + "Row " + seat[0] + " " + "Seat " + seat[1] + "\nWith the name: " + name + "\n");
-		
+
+		System.out.println("The seat you chose was:\n" + "Row " + seat[0] + " " + "Seat " + seat[1]
+				+ "\nWith the name: " + name + "\n");
+
 		theater[seat[0]][seat[1]] = 'X';
-		
+
 		printTheater(theater);
-		
-		
+
+		while (userInputMoreSeats() == 'y') {
+			seat = userInputForSeat();
+			name = userInputForName();
+
+			System.out.println("The seat you chose was:\n" + "Row " + seat[0] + " " + "Seat " + seat[1]
+					+ "\nWith the name: " + name + "\n");
+
+			theater[seat[0]][seat[1]] = 'X';
+
+			printTheater(theater);
+
+			if (userInputMoreSeats() == 'n') {
+				break;
+			}
+
+		}
+
+		input.close();
 
 	}
-	
+
 	public static void printTheater(char arr[][]) {
 		System.out.println("============");
 		System.out.println("SEATS");
 		System.out.println("============\n");
 		System.out.println("   1  2  3  4  5");
 		System.out.println("  --------------");
-		
-		for(int row = 0; row < arr.length ; row++) {
-			
+
+		for (int row = 0; row < arr.length; row++) {
+
 			System.out.print((row + 1) + "|");
-			
-			for(int col = 0; col < arr[row].length; col++) {
-			
+
+			for (int col = 0; col < arr[row].length; col++) {
+
 				System.out.print(" " + arr[row][col] + " ");
 			}
 			System.out.println();
 		}
-		
+
 		System.out.println("\n");
 	}
-	
+
 	public static int[] userInputForSeat() {
 		int[] result = new int[2];
-		
-		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Which seat would you like to reserve");
 
 		System.out.println("Row:");
-		
+
 		int rowInt = input.nextInt();
-		
+
 		result[0] = (rowInt - 1);
-		
+
 		System.out.println("Col:");
-		
+
 		int colInt = input.nextInt();
-		
+
 		result[1] = (colInt - 1);
-		
-		//input.close();
-		
+
 		return result;
 	}
-	
+
 	public static String userInputForName() {
-		Scanner input = new Scanner(System.in);
-		
 		System.out.println("Please enter a name for the seat: ");
-		
+
+		input.nextLine();
+
 		String name = input.nextLine();
-		
-		input.close();
-		
+
 		return name;
 	}
-	
+
+	public static char userInputMoreSeats() {
+		boolean bool = true;
+
+		while (bool == true) {
+
+			System.out.println("Would you like to select another seat (y/n): ");
+
+			String decision = input.nextLine().toLowerCase();
+
+			if (decision.equals("y")) {
+
+				return 'y';
+
+			} else if (decision.equals("n")) {
+
+				return 'n';
+			} else {
+				System.out.println("Please enter y or n.");
+			}
+		}
+
+		return ' ';
+	}
+
 }
---
+++ src/com/cognixia/jump/ProjectScanner.java
@@ -0,0 +1,18 @@
+package com.cognixia.jump;
+
+import java.util.Scanner;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class ProjectScanner {
+	
+	public static void createNewScanner() {
+		Scanner input = new Scanner(System.in);
+	
+	}
+	
+	public static void endScanner() {
+
+	}
+
+}
