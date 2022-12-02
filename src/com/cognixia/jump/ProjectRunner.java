43d0afc1297927e9b155cd6791dc7aa85e6264a3
Micha Bernhard <mib70@pitt.edu>
2022-12-02 09:32:23 -0700

    Add method to print seating chart

--- src/com/cognixia/jump/ProjectRunner.java
+++ src/com/cognixia/jump/ProjectRunner.java
@@ -1,10 +1,43 @@
 package com.cognixia.jump;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ProjectRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		char theater[][] = {
+				{'O', 'O', 'O', 'O', 'O'},
+				{'O', 'O', 'O', 'O', 'O'},
+				{'O', 'O', 'O', 'O', 'O'},
+				{'O', 'O', 'O', 'O', 'O'},
+				{'O', 'O', 'O', 'O', 'O'}
+		};
+		
+		printTheater(theater);
 
 	}
+	
+	public static void printTheater(char arr[][]) {
+		System.out.println("============");
+		System.out.println("SEATS");
+		System.out.println("============\n");
+		System.out.println("  1  2  3  4  5");
+		System.out.println("  --------------");
+		
+		for(int row = 0; row < arr.length ; row++) {
+			
+			System.out.print((row + 1) + "|");
+			
+			for(int col = 0; col < arr[row].length; col++) {
+			
+				System.out.print(" " + arr[row][col] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
 
 }
